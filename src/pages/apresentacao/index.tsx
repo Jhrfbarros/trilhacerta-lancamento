c2e479ec405496adafd382541740aff2a6c53db2
Joao Barros <jhrfbarros@gmail.com>
2022-04-29 13:24:03 -0300

    modificacoes de refatoracao  na page apresentacao

--- src/pages/apresentacao/index.tsx
+++ src/pages/apresentacao/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import HeaderAppBar from '../../components/headerAppBar';
 import FooterAppBar from '../../components/footerAppBar';
 import './index.css'
-import { Container } from '@mui/material';
+import { Grid, Box, Container } from '@mui/material';
 import ButtonSubmit from '../../../src/components/button';
 import InputsForm from '../../../src/components/inputs';
 
@@ -15,28 +15,39 @@ export default function Apresentacao() {
     return (
 
         <Container maxWidth={false} className="default-page">
-            <HeaderAppBar/>
+            <HeaderAppBar />
+
+            <Grid container className="title">
                 <a className="link-page" href="https://trilhacerta.com/">https://trilhacerta.com/</a>
-                
                 <h1 className="title-page">"VOCÊ UM PASSO A FRENTE"</h1>
-                
-                <p className="text-page">O Prof Ricardo Câncio  Santos, mestre em economia aplicada pela USP
-                e ex insider te disponibiliza acesso a informação que ele gostaria
-                de ter tido no início de sua carreira há mais de 20 anos atrás
-                “Acesso a mente de um investidor experiente”
-                Ademais te convida para fazer parte não só de sua comunidade nos
-                mercados financeiros mas também em seu grupo  de network focado em
-                turbinar o seu investimento no mundo real por meio de empreendedorismo
-                e informação de Network
-                </p>
+            </Grid>
+
+            <Grid container className="texto-page">
+                <Grid item xs={12} md={12} lg={12} className='box-texto'>
+                    <p className="text-page">O Prof Ricardo Câncio  Santos, mestre em economia aplicada pela USP
+                    e ex insider te disponibiliza acesso a informação que ele gostaria
+                    de ter tido no início de sua carreira há mais de 20 anos atrás
+                    “Acesso a mente de um investidor experiente”
+                    Ademais te convida para fazer parte não só de sua comunidade nos
+                    mercados financeiros mas também em seu grupo  de network focado em
+                    turbinar o seu investimento no mundo real por meio de empreendedorismo
+                    e informação de Network
+                    </p>
+                </Grid>
+            </Grid>
         
+            <Container>
                 <video className="video-page" controls>
                     <source src={video} type="videochamada/mp4"/>
                 </video>
-        
+            </Container>
+            <Container>
                 <img className="image-page" src={minicurriculo} alt="minicurriculo"></img>
-
+            </Container>
+        
+            <Container>                
                 <h2 className="subtitle-page">FORMULARIO DE CADASTRO</h2>
+                
                 <InputsForm/>
                 <p>Quer divulgar seu produto ou serviço
                 gratuito no nosso Market Place? 
@@ -46,7 +57,8 @@ export default function Apresentacao() {
                     <option value="nao">Não</option>
                 </select>
                 <ButtonSubmit/>
-            
+            </Container>
+
             <FooterAppBar/>    
     
         </Container>
