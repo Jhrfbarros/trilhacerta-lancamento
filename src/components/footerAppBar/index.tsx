627099921a4eef9e5bbbbb1802de00dabb8b7555
Joao Barros <jhrfbarros@gmail.com>
2022-05-02 22:52:48 -0300

    estilizacao do footer

--- src/components/footerAppBar/index.tsx
+++ src/components/footerAppBar/index.tsx
@@ -3,25 +3,25 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+
 
 import './index.css'
 
 
 export default function FooterPage() {
 return (
-    <Box sx={{ flexGrow: 1 }} className='footer-box'>
+    <Box sx={{ flexGrow: 1 }} className='footer'>
       <AppBar className='text-footer' position="static">
-        <Toolbar  variant="dense">
+        
           <Typography variant="h6" color="inherit" component="div">
                 {'Copyright © '}
-            <Link color="inherit" href="/">
-                Ricardo Cancio Santos
+            <Link color="inherit" href="https://github.com/Jhrfbarros">
+                KODE BARROS
             </Link>{' '}
                 {new Date().getFullYear()}
                 {'.'}
           </Typography>
-        </Toolbar>
+        
       </AppBar>
     </Box>
 );
