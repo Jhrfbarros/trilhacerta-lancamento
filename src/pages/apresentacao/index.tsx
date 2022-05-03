0f8038050ccb52f715f2af3a09d57ca0ea38b67d
Lucas de Oliveira e Oliveira <lucasdeoliveiraeoliveira@DEV-MC028.local>
2022-05-02 22:15:32 -0300

    Customizacao gradient

--- src/pages/apresentacao/index.tsx
+++ src/pages/apresentacao/index.tsx
@@ -18,40 +18,44 @@ export default function Apresentacao() {
             
             <HeaderAppBar />
             
-            <Grid container >
-                <video className="video-page" controls>
-                    <source src={video} type="videochamada/mp4"/>
-                </video>
-                <Grid item xs={12} md={12} lg={12} className='box-texto'>
-                    <p className="text-page">O Prof Ricardo Câncio  Santos, mestre em economia aplicada pela USP
-                    e ex insider te disponibiliza acesso a informação que ele gostaria
-                    de ter tido no início de sua carreira há mais de 20 anos atrás
-                    “Acesso a mente de um investidor experiente”
-                    Ademais te convida para fazer parte não só de sua comunidade nos
-                    mercados financeiros mas também em seu grupo  de network focado em
-                    turbinar o seu investimento no mundo real por meio de empreendedorismo
-                    e informação de Network
-                    </p>
+            <Container className='gradient'>
+                <Grid container className='video-container'>
+                    <Grid item xs={12} md={12} lg={12} className='video-grid'>
+                        <video className='player' controls>
+                            <source src={video} type="videochamada/mp4"/>
+                        </video>
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12} className='box-texto'>
+                        <p className="text-page">O Prof Ricardo Câncio  Santos, mestre em economia aplicada pela USP
+                        e ex insider te disponibiliza acesso a informação que ele gostaria
+                        de ter tido no início de sua carreira há mais de 20 anos atrás
+                        “Acesso a mente de um investidor experiente”
+                        Ademais te convida para fazer parte não só de sua comunidade nos
+                        mercados financeiros mas também em seu grupo  de network focado em
+                        turbinar o seu investimento no mundo real por meio de empreendedorismo
+                        e informação de Network
+                        </p>
+                    </Grid>
                 </Grid>
-            </Grid>
+                        
+                <Grid container >
+                    <img className="image-page" src={minicurriculo} alt="minicurriculo"></img>
+                </Grid >
+            
+                <Grid container >                
+                    <h2 className="subtitle-page">FORMULARIO DE CADASTRO</h2>
                     
-            <Grid container >
-                <img className="image-page" src={minicurriculo} alt="minicurriculo"></img>
-            </Grid >
-        
-            <Grid container >                
-                <h2 className="subtitle-page">FORMULARIO DE CADASTRO</h2>
-                
-                <InputsForm/>
-                <p>Quer divulgar seu produto ou serviço
-                gratuito no nosso Market Place? 
-                </p>
-                <select name="select">
-                    <option value="sim" selected>Sim</option>
-                    <option value="nao">Não</option>
-                </select>
-                <ButtonSubmit/>
-            </Grid>
+                    <InputsForm/>
+                    <p>Quer divulgar seu produto ou serviço
+                    gratuito no nosso Market Place? 
+                    </p>
+                    <select name="select">
+                        <option value="sim" selected>Sim</option>
+                        <option value="nao">Não</option>
+                    </select>
+                    <ButtonSubmit/>
+                </Grid>
+            </Container>
 
             <FooterAppBar/>    
     
