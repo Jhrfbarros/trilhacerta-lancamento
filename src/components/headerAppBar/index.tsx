ebeef9351d314d5f2229859b69632c740fab7681
Joao Barros <jhrfbarros@gmail.com>
2022-05-02 22:01:32 -0300

    alinhamento header

--- src/components/headerAppBar/index.tsx
+++ src/components/headerAppBar/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+
 import Typography from '@mui/material/Typography';
 
 
 export default function HeaderAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar className='header' variant="dense">
+      <AppBar className='header' position="static">
+       
                    
-          <Typography className='title' variant="h6" color="inherit" component="div">
+          <Typography className='title' variant="h6" color="inherit" component="p">
             TRILHA CERTA
           </Typography>
           
@@ -19,7 +19,7 @@ export default function HeaderAppBar() {
             Você um passo a frente
           </Typography>
           
-        </Toolbar>
+        
       </AppBar>
     </Box>
   );
