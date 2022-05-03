7b95875dc7823e87a5fc6ea6aff997368dfa9a9b
Lucas de Oliveira e Oliveira <lucasdeoliveiraeoliveira@DEV-MC028.local>
2022-05-02 22:09:20 -0300

    alinhamento header

--- src/components/headerAppBar/index.tsx
+++ src/components/headerAppBar/index.tsx
@@ -1,24 +1,22 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 
 import Typography from '@mui/material/Typography';
 
+import './index.css';
 
 export default function HeaderAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar className='header' position="static">
-       
                    
           <Typography className='title' variant="h6" color="inherit" component="p">
             TRILHA CERTA
           </Typography>
           
           <Typography className='slogan' variant="h6" color="inherit" component="p">
-            Você um passo a frente
+            você um passo a frente
           </Typography>
-          
         
       </AppBar>
     </Box>
