1c6bd88aad83aae4e79b5c81ee3a7febc205f434
Lucas de Oliveira e Oliveira <lucasdeoliveiraeoliveira@DEV-MC028.local>
2022-05-14 01:07:53 -0300

    input mask

--- src/components/inputMasked/index.tsx
+++ src/components/inputMasked/index.tsx
@@ -1,30 +1,33 @@
 import React, { useCallback } from "react";
-import  TextField, {TextFieldProps} from '@mui/material/TextField';
-import InputMask from "react-input-mask";
+import TextField, {TextFieldProps} from '@mui/material/TextField';
 
-
-
-const InputMasked = (props: TextFieldProps) => {
+export default function InputMasked(props:TextFieldProps) {
     
-    const handleChange = (e: TextFieldProps) =>{
-        //(11)9999-9999
-        console.log("AQUI");
+    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) =>{
+
+        const type = e.currentTarget.attributes.getNamedItem("type")?.value;
+        let value = e.currentTarget.value;
+
+        switch (type) {
+            case "nome":
+                value = value.replace(/[1234567890`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+                break;
+            case "telefone":
+                //Mask Format: (11)9999-9999
+                e.currentTarget.maxLength = 11;
+                value = value.replace(/\D/g, "");
+                value = value.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
+                break;
+            default:
+                break;
+        }
+        e.currentTarget.value = value;
         
-        e.inputProps={maxLength: 2};
-        let value = e.value;
-        console.log(value);
-        
-    };
+    }, []);
 
     return(
         <div>
-            <InputMask alwaysShowMask mask="99/99/9999">
-                <TextField type="tel" {...props} onChange={handleChange} value="teste" />
-            </InputMask>
+            <TextField type="tel" {...props} onChange={handleChange} />
         </div>
     )
-
-    
 };
-
-export default InputMasked;
